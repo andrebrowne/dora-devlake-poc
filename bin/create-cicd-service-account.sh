e82421d47ed8cace5f20db94cf3217ab865bd14f
andrebrowne <42154231+andrebrowne@users.noreply.github.com>
2024-10-28 10:49:31 -0400

    Final cleanup

--- bin/create-cicd-service-account.sh
+++ bin/create-cicd-service-account.sh
@@ -2,11 +2,17 @@
 
 set -x
 
+SA_NAME=${1:-jenkins}
+NAMESPACE=${2:-default}
+
 eval $(minikube -p minikube docker-env)
-kubectl -n default create serviceaccount ${1:-jenkins}-robot
-kubectl -n default create rolebinding ${1:-jenkins}-robot-binding --clusterrole=cluster-admin --serviceaccount=default:${1:-jenkins}-robot
-kubectl -n default create clusterrolebinding ${1:-jenkins}-robot-binding --clusterrole=cluster-admin --serviceaccount=default:${1:-jenkins}-robot
-#kubectl -n default get serviceaccount ${1:-jenkins}-robot -o go-template --template='{{range .secrets}}{{.name}}{{"\n"}}{{end}}'
-#kubectl -n default get serviceaccount ${1:-jenkins}-robot -o yaml
-echo "Use token to configure ${1:-jenkins}:" 
-kubectl create token ${1:-jenkins}-robot
+
+kubectl -n ${NAMESPACE} create serviceaccount ${SA_NAME}-robot
+kubectl -n ${NAMESPACE} create rolebinding ${SA_NAME}-robot-binding --clusterrole=cluster-admin --serviceaccount=${NAMESPACE}:${SA_NAME}-robot
+kubectl -n ${NAMESPACE} create clusterrolebinding ${SA_NAME}-robot-binding --clusterrole=cluster-admin --serviceaccount=${NAMESPACE}:${SA_NAME}-robot
+#kubectl -n ${NAMESPACE} get serviceaccount ${SA_NAME}-robot -o go-template --template='{{range .secrets}}{{.name}}{{"\n"}}{{end}}'
+#kubectl -n ${NAMESPACE} get serviceaccount ${SA_NAME}-robot -o yaml
+
+echo "Use token to configure ${SA_NAME}:" 
+
+kubectl create token ${SA_NAME}-robot
